47778e2199d40aa4919654499d4b6935191e2441
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-03-21 22:48:20 +0100

    expanded dto's and interfaces

--- src/main/java/at/fhv/ss22/ea/f/communication/api/ProductSearchService.java
+++ src/main/java/at/fhv/ss22/ea/f/communication/api/ProductSearchService.java
@@ -1,12 +1,18 @@
 package at.fhv.ss22.ea.f.communication.api;
 
 
+import at.fhv.ss22.ea.f.communication.dto.ProductDetailsDTO;
 import at.fhv.ss22.ea.f.communication.dto.ProductOverviewDTO;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface ProductSearchService extends Remote {
+
+    Optional<ProductDetailsDTO> productById(UUID productId);
+
     List<ProductOverviewDTO> fullTextSearch(String query) throws RemoteException;
 }
