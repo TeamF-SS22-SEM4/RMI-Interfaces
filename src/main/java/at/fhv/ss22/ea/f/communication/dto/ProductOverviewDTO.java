e50902d040dae7cb885d10d842ee66879a9df848
Smightym8 <michaelspiegel@live.at>
2022-03-21 07:48:35 +0100

    Created folder for dtos and moved new ProductOverviewDTO

---
+++ src/main/java/at/fhv/ss22/ea/f/communication/dto/ProductOverviewDTO.java
@@ -0,0 +1,56 @@
+package at.fhv.ec.javafxclient.application.dto;
+
+import java.util.Objects;
+
+public class ProductOverviewDTO {
+    private String name;
+    private String artistName;
+    private String releaseYear;
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    public String artistName() {
+        return this.artistName;
+    }
+
+    public String releaseYear() {
+        return this.releaseYear;
+    }
+
+    public static class Builder {
+        private final ProductOverviewDTO instance;
+
+        private Builder() {
+            this.instance = new ProductOverviewDTO();
+        }
+
+        public Builder withName(String name) {
+            this.instance.name = name;
+            return this;
+        }
+
+        public Builder withArtistName(String artistName) {
+            this.instance.artistName = artistName;
+            return this;
+        }
+
+        public Builder withReleaseYear(String releaseYear) {
+            this.instance.releaseYear = releaseYear;
+            return this;
+        }
+
+        public ProductOverviewDTO build() {
+            Objects.requireNonNull(this.instance.name, "name must be set in ProductOverviewDTO");
+            Objects.requireNonNull(this.instance.artistName, "artistFirstname must be set in ProductOverviewDTO");
+            Objects.requireNonNull(this.instance.releaseYear, "releaseYear must be set in ProductOverviewDTO");
+
+            return this.instance;
+        }
+    }
+}
