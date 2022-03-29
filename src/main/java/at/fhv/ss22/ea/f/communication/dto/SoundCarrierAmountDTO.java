2458ef1371b6af26a404eb72fed214d2d615a79d
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-03-29 16:32:49 +0200

    added buying stuff

---
+++ src/main/java/at/fhv/ss22/ea/f/communication/dto/SoundCarrierAmountDTO.java
@@ -0,0 +1,36 @@
+package at.fhv.ss22.ea.f.communication.dto;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class SoundCarrierAmountDTO implements Serializable {
+    private int amount;
+    private UUID carrierId;
+
+    public int getAmount() {
+        return amount;
+    }
+    public UUID getCarrierId() {
+        return carrierId;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+    public static class Builder {
+        private SoundCarrierAmountDTO instance = new SoundCarrierAmountDTO();
+
+        public Builder withAmount(int amount) {
+            this.instance.amount = amount;
+            return this;
+        }
+        public Builder withCarrierId(UUID carrierId) {
+            this.instance.carrierId = carrierId;
+            return this;
+        }
+        public SoundCarrierAmountDTO build() {
+            return this.instance;
+        }
+    }
+
+}
