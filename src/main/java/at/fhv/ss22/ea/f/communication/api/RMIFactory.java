50eb9d72cbe1e9e516a62ab8a7eb7abed5f2c484
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-03-29 16:50:28 +0200

    buyingService in factory

--- src/main/java/at/fhv/ss22/ea/f/communication/api/RMIFactory.java
+++ src/main/java/at/fhv/ss22/ea/f/communication/api/RMIFactory.java
@@ -5,4 +5,6 @@ import java.rmi.RemoteException;
 
 public interface RMIFactory extends Remote {
     ProductSearchService getProductSearchService() throws RemoteException;
+
+    BuyingService getBuyingService() throws RemoteException;
 }
