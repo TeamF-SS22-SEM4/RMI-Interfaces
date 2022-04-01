1cfa98b2ed35818b2d3e3c9689ce477e70e1df7d
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-04-01 09:04:38 +0200

    customer service

---
+++ src/main/java/at/fhv/ss22/ea/f/communication/api/CustomerService.java
@@ -0,0 +1,19 @@
+package at.fhv.ss22.ea.f.communication.api;
+
+import at.fhv.ss22.ea.f.communication.dto.CustomerDTO;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomerService extends Remote {
+
+    Optional<CustomerDTO> customerById(UUID uuid) throws RemoteException;
+
+    List<CustomerDTO> customerListByIds(List<UUID> uuidList) throws RemoteException;
+
+    List<CustomerDTO> search(String query) throws RemoteException;
+
+}
