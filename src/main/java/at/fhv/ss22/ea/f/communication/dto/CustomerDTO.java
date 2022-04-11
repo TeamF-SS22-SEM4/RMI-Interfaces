b0a2207e2a288c92182a7f7f949f8d3bfc2eef69
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-04-11 14:10:38 +0200

    serial Version

--- src/main/java/at/fhv/ss22/ea/f/communication/dto/CustomerDTO.java
+++ src/main/java/at/fhv/ss22/ea/f/communication/dto/CustomerDTO.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class CustomerDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private UUID customerId;
     private String givenName;
     private String familyName;
