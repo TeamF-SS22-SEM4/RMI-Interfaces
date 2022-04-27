184cf2714bb5daf9157a7323ec7b365881f88060
Smightym8 <michaelspiegel@live.at>
2022-04-27 21:28:37 +0200

    Added methods for last view of messages

--- src/main/java/at/fhv/ss22/ea/f/communication/api/MessagingService.java
+++ src/main/java/at/fhv/ss22/ea/f/communication/api/MessagingService.java
@@ -6,6 +6,7 @@ import at.fhv.ss22.ea.f.communication.exception.SessionExpired;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MessagingService extends Remote {
@@ -13,4 +14,8 @@ public interface MessagingService extends Remote {
     boolean publish(String sessionId, MessageDTO message) throws RemoteException, SessionExpired, NoPermissionForOperation;
 
     List<String> getSubscribedTopics(String sessionId) throws RemoteException, SessionExpired, NoPermissionForOperation;
+
+    void updateLastViewed(LocalDateTime lastViewedMessages);
+
+    LocalDateTime getLastViewed();
 }
